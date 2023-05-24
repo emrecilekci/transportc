0c95febff795e3407d960589617a22a97d69b706
emrecilekci19 <54431589+emrecilekci19@users.noreply.github.com>
2023-05-24 15:17:58 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import transport as migrationmanager
 
-newmigration = migrationmanager.transportcb('10.85.237.139' , '10.85.235.82', 'USER' , 'PASS!')
+newmigration = migrationmanager.transportcb('**' , '**', 'USER' , 'PASS!')
 
 
 Galatasaray+-345
